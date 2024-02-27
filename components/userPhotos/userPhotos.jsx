7ef8eaea5f5776143c049caffe27858b706c5071
Abheek Ranjan Das <35596298+Gummykit@users.noreply.github.com>
2024-02-26 22:15:03 -0500

    Update userPhotos.jsx

--- components/userPhotos/userPhotos.jsx
+++ components/userPhotos/userPhotos.jsx
@@ -1,34 +1,119 @@
-import React from 'react';
-import {
-  Typography
-} from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { Typography, Button } from '@mui/material';
+import { Link } from 'react-router-dom';
 import './userPhotos.css';
+import TopBar from '../topBar/TopBar';
+import FetchModel from '../../lib/fetchModelData';
 
+function UserPhotos({ match }) {
+  const [photos, setPhotos] = useState([]);
+  const [user, setUser] = useState(null);
+  const [comment, setComment] = useState(null);
 
-/**
- * Define UserPhotos, a React componment of project #5
- */
-class UserPhotos extends React.Component {
-  constructor(props) {
-    super(props);
+  useEffect(() => {
+    fetchUserPhotosAndDetails();
+  }, [match.params.userId]);
 
-  }
+  const fetchUserPhotosAndDetails = async () => {
+    const { userId } = match.params;
 
-  render() {
-    return (
-      <Typography variant="body1">
-      This should be the UserPhotos view of the PhotoShare app. Since
-      it is invoked from React Router the params from the route will be
-      in property match. So this should show details of user:
-      {this.props.match.params.userId}. You can fetch the model for the user from
-      window.models.photoOfUserModel(userId):
-        <Typography variant="caption">
-          {JSON.stringify(window.models.photoOfUserModel(this.props.match.params.userId))}
-        </Typography>
+    try {
+      const photosResponse = await FetchModel(`/photosOfUser/${userId}`);
+      setPhotos(photosResponse.data);
+
+      const userDetailsResponse = await FetchModel(`/user/${userId}`);
+      const userDetails = userDetailsResponse.data;
+
+      setUser(userDetails);
+      setComment(userDetails ? userDetails.comment : null);
+    } catch (error) {
+      console.error('Error fetching user data:', error);
+    }
+  };
+
+  const topNameValue = user ? `User photos for ${user.first_name} ${user.last_name}` : '';
+
+  return (
+    <div>
+      <TopBar topName={topNameValue} />
+
+      <Button
+        component={Link}
+        to={`/users/${match.params.userId}`}
+        variant="contained"
+        className="ButtonLink"
+      >
+        User Details
+      </Button>
+
+      <Typography variant="h4" className="UserPhotosHeader">
+        User Photos
       </Typography>
 
-    );
-  }
+      <div className="photo-list">
+        {photos.map((photo) => (
+          <div key={photo._id} className="photo-item">
+            <img
+              src={`/images/${photo.file_name}`}
+              alt={`User ${match.params.userId}'s Photo`}
+              className="photo-image"
+            />
+
+            <div className="user-photo-box" style={{ marginTop: '16px' }}>
+              <Typography variant="caption" className="user-photo-title">
+                Date Taken
+              </Typography>
+
+              <Typography variant="body1" className="user-photo-value">
+                {photo.date_time}
+              </Typography>
+            </div>
+
+            {photo.comments && photo.comments.length > 0 && (
+              <div>
+                <p style={{ margin: 0, fontWeight: 'bold' }}>Comments:</p>
+
+                {photo.comments.map((comment) => (
+                  <div key={comment._id} className="user-photo-box" style={{ marginTop: '16px' }}>
+                    <p>{comment.comment}</p>
+                    <p>
+                      <b>Commented ON:</b> {comment.date_time}
+                    </p>
+                    <p>
+                      <b>Commented BY:</b>
+                      <Link to={`/users/${comment.user._id}`}>
+                        {comment.user.first_name} {comment.user.last_name}
+                      </Link>
+                    </p>
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
+
+      {user ? (
+        <div>
+          {comment && (
+            <div className="user-photo-box" style={{ marginTop: '16px' }}>
+              <Typography variant="caption" className="user-photo-title">
+                Comment
+              </Typography>
+
+              <Typography variant="body1" className="user-photo-value">
+                {comment}
+              </Typography>
+            </div>
+          )}
+        </div>
+      ) : (
+        <Typography variant="body1" className="user-detail-box loading-text">
+          Loading user details...
+        </Typography>
+      )}
+    </div>
+  );
 }
 
 export default UserPhotos;
