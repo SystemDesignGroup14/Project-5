e2d13f129c80eae8aa6a0bf84f9da9aaaffef27b
Aravinda Reddy <aravind4532@gmail.com>
2024-02-26 22:56:28 -0500

    Here is more modular code with same functionality, also i have fixed issue of topbar

--- components/userDetail/userDetail.jsx
+++ components/userDetail/userDetail.jsx
@@ -1,96 +1,76 @@
 import React from 'react';
-import { Typography, Button} from '@mui/material';
+import { Typography, Button } from '@mui/material';
 import { Link } from 'react-router-dom';
 import './userDetail.css';
-import FetchModel from '../../lib/fetchModelData'; 
+import FetchModel from '../../lib/fetchModelData';
+import TopBar from '../topBar/TopBar';
 
 class UserDetail extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      user: null,
-    };
-  }
+  state = {
+    user: null,
+    selectedUser: '',
+  };
 
   componentDidMount() {
     this.getUserDetails();
   }
 
   componentDidUpdate(prevProps) {
-    const { userId }=this.props.match.params;
-    if (prevProps.match.params.userId !== userId || !this.state.user) {
+    const { userId } = this.props.match.params;
+    if (prevProps.match.params.userId !== userId) {
       this.getUserDetails();
     }
   }
 
-  getUserDetails() {
-    const { userId }=this.props.match.params;
+  getUserDetails = () => {
+    const { userId } = this.props.match.params;
     FetchModel(`/user/${userId}`)
       .then((response) => {
-        this.setState({ user: response.data });
+        this.setState({
+          user: response.data,
+          selectedUser: "User Details: "+response.data.first_name 
+        });
       })
-      .catch((error) => {
-        console.error('There is an error:', error);
-      });
-  }
+      .catch((error) => console.error('There is an error:', error));
+  };
+
+  renderDetail = (label, value) => (
+    <div className="box">
+      <Typography variant="body1" className="heading">
+        {label}
+      </Typography>
+      <Typography variant="body1" className="description">
+        {value}
+      </Typography>
+    </div>
+  );
 
   render() {
-    const { user } = this.state;
+    const { user, selectedUser } = this.state;
+
     return (
       <div>
+        <TopBar currentpageLabelOnTopBar={selectedUser} />
         {user ? (
           <div>
             <div>
-              <Button component={Link} to={`/photos/${user._id}`} variant="contained" color="primary">
-                  User Photos
+              <Button
+                component={Link}
+                to={`/photos/${user._id}`}
+                variant="contained"
+                color="primary"
+              >
+                User Photos
               </Button>
             </div>
-
-            <div className="box-first box">
-              <Typography variant="body1" className="heading">
-                First Name
-              </Typography>
-              <Typography variant="body1" className="description">
-                {user.first_name}
-              </Typography>
-            </div>
-            <div className="box">
-              <Typography variant="body1" className="heading">
-                Last Name
-              </Typography>
-              <Typography variant="body1" className="description">
-                {user.last_name}
-              </Typography>
-            </div>
-            <div className="box">
-              <Typography variant="body1" className="heading">
-                Location
-              </Typography>
-              <Typography variant="body1" className="description">
-                {user.location}
-              </Typography>
-            </div>
-            <div className="box">
-              <Typography variant="body1" className="heading">
-                Description
-              </Typography>
-              <Typography variant="body1" className="description">
-                {user.description}
-              </Typography>
-            </div>
-            <div className="box">
-              <Typography variant="body1" className="heading">
-                Occupation
-              </Typography>
-              <Typography variant="body1" className="description">
-                {user.occupation}
-              </Typography>
-            </div>
+            {this.renderDetail('First Name', user.first_name)}
+            {this.renderDetail('Last Name', user.last_name)}
+            {this.renderDetail('Location', user.location)}
+            {this.renderDetail('Description', user.description)}
+            {this.renderDetail('Occupation', user.occupation)}
           </div>
         ) : (
-          <Typography variant="body1" className="box">
-            Loading user details
-          </Typography>
+          <Typography variant="body1" className="box">Loading user details...</Typography>
         )}
       </div>
     );
