a2dcd2e7ac762f19c7b45176a4b7bec3ed1a92e3
Aravinda Reddy <aravind4532@gmail.com>
2024-02-29 17:26:20 -0500

    Sprint 1 : Code review for Top Bar so made it more modular

--- components/topBar/TopBar.jsx
+++ components/topBar/TopBar.jsx
@@ -1,43 +1,57 @@
-import React, { useState, useEffect } from 'react';
+import React, { Component } from 'react';
 import { AppBar, Toolbar, Typography } from '@mui/material';
 import './TopBar.css';
 import FetchModel from '../../lib/fetchModelData';
 
-const TopBar = (props) => {
-  const [appVersion, setAppVersion] = useState(undefined);
-
-  useEffect(() => {
-    const fetchAppVersion = async () => {
-      try {
-        const response = await FetchModel("/test/info");
-        setAppVersion(response.data);
-      } catch (error) {
-        console.error('Error fetching app version:', error);
-      }
+class TopBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      appVersion: undefined,
     };
+  }
 
-    if (appVersion === undefined) {
-      fetchAppVersion();
+  componentDidMount() {
+    this.fetchAppVersion();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.appVersion === undefined) {
+      this.fetchAppVersion();
     }
-  }, [appVersion]);
+  }
 
-  return appVersion ? (
-    <AppBar className="topbar-appBar" position="absolute">
-      <Toolbar className='topbar'>
-        <Typography variant="h5" color="inherit">
-          GROUP12
-        </Typography>
-        <Typography variant="h5" color="inherit">
-          {props.topName}
-        </Typography>
-        <Typography variant="h5" component="div" color="inherit">
-          Version: {appVersion.__v}
-        </Typography>
-      </Toolbar>
-    </AppBar>
-  ) : (
-    <div />
-  );
-};
+  fetchAppVersion = async () => {
+    try {
+      const response = await FetchModel("/test/info");
+      this.setState({ appVersion: response.data });
+    } catch (error) {
+      console.error('Error fetching app version:', error);
+    }
+  };
+
+  renderAppBar = () => {
+    const { appVersion } = this.state;
+    return (
+      <AppBar className="topbar-appBar" position="absolute">
+        <Toolbar className='topbar'>
+          <Typography variant="h5" color="inherit">
+            SSDI Group 12
+          </Typography>
+          <Typography variant="h5" color="inherit">
+          {this.props.currentpageLabelOnTopBar ? this.props.currentpageLabelOnTopBar : " "}
+          </Typography>
+          <Typography variant="h5" component="div" color="inherit">
+            Version: {appVersion.__v}
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    );
+  };
+
+  render() {
+    return this.state.appVersion ? this.renderAppBar() : <div />;
+  }
+}
 
 export default TopBar;
