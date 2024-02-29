03f47bfb577feee4091bf1a44a16d159cd7bd5dc
Ramgopal Hyndav Bollepalli <143753798+Ramgopalhyndavbollepalli@users.noreply.github.com>
2024-02-28 22:18:27 -0500

    Update TopBar.jsx
    Added FetchModel for version retrieval in TopBar component

--- components/topBar/TopBar.jsx
+++ components/topBar/TopBar.jsx
@@ -1,33 +1,43 @@
-import React from 'react';
-import {
-  AppBar, Toolbar, Typography
-} from '@mui/material';
+import React, { useState, useEffect } from 'react';
+import { AppBar, Toolbar, Typography } from '@mui/material';
 import './TopBar.css';
+import FetchModel from '../../lib/fetchModelData';
 
-/**
- * Define TopBar, a React componment of project #5
- */
-class TopBar extends React.Component {
-  constructor(props) {
-    super(props);
-    
-  }
+const TopBar = (props) => {
+  const [appVersion, setAppVersion] = useState(undefined);
 
-  render() {
-    return (
-      <AppBar className="topbar-appBar" position="absolute">
-        <Toolbar className="appbar">
-          <Typography variant="h5" color="inherit">
-              Ssdi Group 12
-          </Typography>
-          <Typography variant="h5" color="inherit">
-              {this.props.currentpageLabelOnTopBar ? this.props.currentpageLabelOnTopBar : " "}
-          </Typography>
-          
-        </Toolbar>
-      </AppBar>
-    );
-  }
-}
+  useEffect(() => {
+    const fetchAppVersion = async () => {
+      try {
+        const response = await FetchModel("/test/info");
+        setAppVersion(response.data);
+      } catch (error) {
+        console.error('Error fetching app version:', error);
+      }
+    };
+
+    if (appVersion === undefined) {
+      fetchAppVersion();
+    }
+  }, [appVersion]);
+
+  return appVersion ? (
+    <AppBar className="topbar-appBar" position="absolute">
+      <Toolbar className='topbar'>
+        <Typography variant="h5" color="inherit">
+          GROUP12
+        </Typography>
+        <Typography variant="h5" color="inherit">
+          {props.topName}
+        </Typography>
+        <Typography variant="h5" component="div" color="inherit">
+          Version: {appVersion.__v}
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  ) : (
+    <div />
+  );
+};
 
 export default TopBar;
