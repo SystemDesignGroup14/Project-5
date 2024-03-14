38af267e1b5464c1d1cd5f7be38325387d81d470
Aravinda Reddy <aravind4532@gmail.com>
2024-03-13 20:50:26 -0400

    Sprint 3 : Us 1 : Database integration and end point conversion from static files to mongo db

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
-# Project Description: Sprint 1 - Photo Sharing Application
-**Overview:**
-The project focuses on developing a photo-sharing web application using ReactJS and Material-UI components. The application aims to provide users with the ability to view, upload, and comment on photos, organized by users. Sprint 1 lays the groundwork for the application's core functionality and structure following the Scrum methodology for agile development.
-
 # Group 12 Scrum Team
 ## Product Owner
 Abheek Ranjan Das
@@ -11,6 +7,66 @@ Aravinda Reddy Gangalakunta
 - Gokul Reddy Devarapalli
 - Ramgopal Hyndav Bollepalli
 - Danny Bylla
+
+# Project 6: Full Stack Photo Sharing App
+
+## Introduction
+
+This project enhances our photo sharing application by integrating MongoDB for data persistence, allowing dynamic content management. We've encapsulated the environment setup using Docker Compose for ease of development and deployment.
+
+## Setup
+
+### Requirements
+
+- Docker and Docker Compose optional
+- Node.js (for local development)
+
+### Getting Started
+
+1. **Docker Compose Setup**: To spin up the MongoDB database , simply run:
+
+```bash
+docker-compose up -d
+```
+
+This command starts all required services as defined in our `docker-compose.yml` file, including the MongoDB instance and express.
+
+
+
+You should see containers for both the MongoDB database and express.
+
+
+## API Overview
+
+With the integration of MongoDB, our app's API now directly interacts with a live database, enhancing data management and scalability. Here's a brief overview:
+
+- **User List (`/user/list`)**: Fetches a list of all users from the MongoDB `User` collection, displaying essential details like name and occupation.
+
+- **User Details (`/user/:id`)**: Retrieves detailed information about a specific user by their ID, including their photos and comments.
+
+- **Photos of User (`/photosOfUser/:id`)**: Returns all photos uploaded by a specific user, along with comments on each photo, leveraging MongoDB for data retrieval.
+
+### Example: Fetching User Details
+
+```javascript
+app.get("/user/:id", async (request, response) => {
+  try {
+    const user = await User.findById(request.params.id);
+    if (!user) {
+      return response.status(404).send("User not found");
+    }
+    response.json(user);
+  } catch (error) {
+    response.status(500).send("Internal Server Error");
+  }
+});
+```
+
+This endpoint demonstrates how we interact with MongoDB to fetch and return user details, ensuring a dynamic and responsive application experience.
+
+
+
+
 
 
 ### Problem 1: Create the Photo Sharing Application
--- webServer.js
+++ webServer.js
@@ -1,92 +1,278 @@
-'use strict';
-
-/*
- * A simple Node.js program for exporting the current working directory via a webserver listing
- * on a hard code (see portno below) port. To start the webserver run the command:
+/**
+ * This builds on the webServer of previous projects in that it exports the
+ * current directory via webserver listing on a hard code (see portno below)
+ * port. It also establishes a connection to the MongoDB named 'project6'.
+ *
+ * To start the webserver run the command:
  *    node webServer.js
  *
- * Note that anyone able to connect to localhost:3001 will be able to fetch any file accessible
- * to the current user in the current directory or any of its children.
+ * Note that anyone able to connect to localhost:portNo will be able to fetch
+ * any file accessible to the current user in the current directory or any of
+ * its children.
+ *
+ * This webServer exports the following URLs:
+ * /            - Returns a text status message. Good for testing web server
+ *                running.
+ * /test        - Returns the SchemaInfo object of the database in JSON format.
+ *                This is good for testing connectivity with MongoDB.
+ * /test/info   - Same as /test.
+ * /test/counts - Returns the population counts of the cs collections in the
+ *                database. Format is a JSON object with properties being the
+ *                collection name and the values being the counts.
+ *
+ * The following URLs need to be changed to fetch there reply values from the
+ * database:
+ * /user/list         - Returns an array containing all the User objects from
+ *                      the database (JSON format).
+ * /user/:id          - Returns the User object with the _id of id (JSON
+ *                      format).
+ * /photosOfUser/:id  - Returns an array with all the photos of the User (id).
+ *                      Each photo should have all the Comments on the Photo
+ *                      (JSON format).
  */
 
-/* jshint node: true */
+const mongoose = require("mongoose");
+mongoose.Promise = require("bluebird");
 
-var express = require('express');
+const async = require("async");
 
-var portno = 3000;   // Port number to use
+const express = require("express");
+const app = express();
 
-var app = express();
+// Load the Mongoose schema for User, Photo, and SchemaInfo
+const User = require("./schema/user.js");
+const Photo = require("./schema/photo.js");
+const SchemaInfo = require("./schema/schemaInfo.js");
 
-var models = require('./modelData/photoApp.js').models;
+// XXX - Your submission should work without this line. Comment out or delete
+// this line for tests and before submission!
+const models = require("./modelData/photoApp.js").models;
+mongoose.set("strictQuery", false);
+mongoose.connect("mongodb://root:example@127.0.0.1:27017/project6?authSource=admin", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => console.log('MongoDB connected...'))
+.catch(err => console.error(err));
 
-// We have the express static module (http://expressjs.com/en/starter/static-files.html) do all
-// the work for us.
+const db= mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', function() {
+  console.log("We're connected to the database!");
+});
+
+// We have the express static module
+// (http://expressjs.com/en/starter/static-files.html) do all the work for us.
 app.use(express.static(__dirname));
 
-app.get('/', function (request, response) {
-  response.send('Simple web server of files from ' + __dirname);
+app.get("/", function (request, response) {
+  response.send("Simple web server of files from " + __dirname);
 });
 
-app.get('/test/:p1', function (request, response) {
-  // Express parses the ":p1" from the URL and returns it in the request.params objects.
-  var param = request.params.p1;
-  console.log('/test called with param1 = ', param);
-  if (param !== "info") {
-    console.error("Nothing to be done for param: ", param);
-    response.status(400).send('Not found');
-    return;
+/**
+ * Use express to handle argument passing in the URL. This .get will cause
+ * express to accept URLs with /test/<something> and return the something in
+ * request.params.p1.
+ * 
+ * If implement the get as follows:
+ * /test        - Returns the SchemaInfo object of the database in JSON format.
+ *                This is good for testing connectivity with MongoDB.
+ * /test/info   - Same as /test.
+ * /test/counts - Returns an object with the counts of the different collections
+ *                in JSON format.
+ */
+app.get("/test/:p1", function (request, response) {
+  // Express parses the ":p1" from the URL and returns it in the request.params
+  // objects.
+  console.log("/test called with param1 = ", request.params.p1);
+
+  const param = request.params.p1 || "info";
+
+  if (param === "info") {
+    // Fetch the SchemaInfo. There should only one of them. The query of {} will
+    // match it.
+    SchemaInfo.find({}, function (err, info) {
+      if (err) {
+        // Query returned an error. We pass it back to the browser with an
+        // Internal Service Error (500) error code.
+        console.error("Error in /user/info:", err);
+        response.status(500).send(JSON.stringify(err));
+        return;
+      }
+      if (info.length === 0) {
+        // Query didn't return an error but didn't find the SchemaInfo object -
+        // This is also an internal error return.
+        response.status(500).send("Missing SchemaInfo");
+        return;
+      }
+
+      // We got the object - return it in JSON format.
+      console.log("SchemaInfo", info[0]);
+      response.end(JSON.stringify(info[0]));
+    });
+  } else if (param === "counts") {
+    // In order to return the counts of all the collections we need to do an
+    // async call to each collections. That is tricky to do so we use the async
+    // package do the work. We put the collections into array and use async.each
+    // to do each .count() query.
+    const collections = [
+      { name: "user", collection: User },
+      { name: "photo", collection: Photo },
+      { name: "schemaInfo", collection: SchemaInfo },
+    ];
+    async.each(
+      collections,
+      function (col, done_callback) {
+        col.collection.countDocuments({}, function (err, count) {
+          col.count = count;
+          done_callback(err);
+        });
+      },
+      function (err) {
+        if (err) {
+          response.status(500).send(JSON.stringify(err));
+        } else {
+          const obj = {};
+          for (let i = 0; i < collections.length; i++) {
+            obj[collections[i].name] = collections[i].count;
+          }
+          response.end(JSON.stringify(obj));
+        }
+      }
+    );
+  } else {
+    // If we know understand the parameter we return a (Bad Parameter) (400)
+    // status.
+    response.status(400).send("Bad param " + param);
   }
+});
+
+/**
+ * URL /user/list - Returns all the User objects.
+ */
+// app.get("/user/list", function (request, response) {
+//   response.status(200).send(models.userListModel());
+// });
+app.get("/user/list", function (request, response) {
+  User.find({}, function (err, users) {
+    if (err) {
+      console.error("Error fetching users:", err);
+      response.status(500).send("Internal Server Error");
+      return;
+    }
+    response.status(200).json(users);
+  });
+});
+
+
+/**
+ * URL /user/:id - Returns the information for User (id).
+ */
+// app.get("/user/:id", function (request, response) {
+//   const id = request.params.id;
+//   const user = models.userModel(id);
+//   if (user === null) {
+//     console.log("User with _id:" + id + " not found.");
+//     response.status(400).send("Not found");
+//     return;
+//   }
+//   response.status(200).send(user);
+// });
+app.get("/user/:id", function (request, response) {
+  const userId = request.params.id;
+  User.findById(userId, function (err, user) {
+    if (err) {
+      console.error("Error fetching user:", err);
+      response.status(500).send("Internal Server Error");
+      return;
+    }
+    if (!user) {
+      console.log("User with _id:" + userId + " not found.");
+      response.status(404).send("User not found");
+      return;
+    }
+    response.status(200).json(user);
+  });
+});
+
+
+// /**
+//  * URL /photosOfUser/:id - Returns the Photos for User (id).
+//  */
+// app.get("/photosOfUser/:id", function (request, response) {
+//   const id = request.params.id;
+//   const photos = models.photoOfUserModel(id);
+//   if (photos.length === 0) {
+//     console.log("Photos for user with _id:" + id + " not found.");
+//     response.status(400).send("Not found");
+//     return;
+//   }
+//   response.status(200).send(photos);
+// });
+
+//This is requests mongo for fetching data
+
+app.get('/photosOfUser/:id', async function (request, response) {
+  // Begin by extracting the user ID from the URL parameters.
+  const userId = request.params.id;
   
-  var info = models.schemaInfo();
-  
-  // Query didn't return an error but didn't find the SchemaInfo object - This
-  // is also an internal error return.
-  if (info.length === 0) {
-    response.status(500).send('Missing SchemaInfo');
-    return;
+  // First, verify the existence of the user associated with the provided ID.
+  const userExists = await User.findById(userId);
+  if (!userExists) {
+    // If no matching user is found, inform the requester with a 404 Not Found response.
+    return response.status(404).send("User not found");
   }
-  response.status(200).send(info);
+
+  // Proceed to retrieve all photos uploaded by the identified user.
+  Photo.find({ user_id: userId })
+    .lean() // Optimize by converting Mongoose documents into plain JavaScript objects for more straightforward manipulation.
+    .exec(async function (err, photos) {
+      if (err) {
+        // Log and report any issues encountered during the photo retrieval process.
+        console.error("Encountered an error fetching photos:", err);
+        return response.status(500).send("Internal Server Error");
+      }
+      
+      if (photos.length === 0) {
+        // Notify the requester if the specified user has no associated photos.
+        return response.status(404).send("Photos not found");
+      }
+
+      try {
+        // Iterate through each photo to enhance comment details.
+        for (let photo of photos) {
+          if (photo.comments) {
+            for (let comment of photo.comments) {
+              // Fetch and append commenter details for each comment, replacing generic user IDs with specific user information.
+              const commentingUser = await User.findById(comment.user_id);
+              if (commentingUser) {
+                // Update the comment structure to include the commenter's full details.
+                comment.user = commentingUser;
+              }
+              // Remove the user_id field from comments to streamline the response and focus on relevant details.
+              delete comment.user_id;
+            }
+          }
+        }
+
+        // After processing, return the enriched set of photos and comments.
+        response.status(200).json(photos);
+      } catch (error) {
+        // Should processing fail, log the error and provide an appropriate server response.
+        console.error("A processing error occurred with photos:", error);
+        response.status(500).send("Internal Server Error");
+      }
+    });
 });
 
-/*
- * URL /user/list - Return all the User object.
- */
-app.get('/user/list', function (request, response) {
-  response.status(200).send(models.userListModel());
-  return;
-});
-
-/*
- * URL /user/:id - Return the information for User (id)
- */
-app.get('/user/:id', function (request, response) {
-  var id = request.params.id;
-  var user = models.userModel(id);
-  if (user === null) {
-    console.log('User with _id:' + id + ' not found.');
-    response.status(400).send('Not found');
-    return;
-  }
-  response.status(200).send(user);
-  return;
-});
-
-/*
- * URL /photosOfUser/:id - Return the Photos for User (id)
- */
-app.get('/photosOfUser/:id', function (request, response) {
-  var id = request.params.id;
-  var photos = models.photoOfUserModel(id);
-  if (photos.length === 0) {
-    console.log('Photos for user with _id:' + id + ' not found.');
-    response.status(400).send('Not found');
-    return;
-  }
-  response.status(200).send(photos);
-});
 
 
-var server = app.listen(portno, function () {
-  var port = server.address().port;
-  console.log('Listening at http://localhost:' + port + ' exporting the directory ' + __dirname);
+
+const server = app.listen(3000, function () {
+  const port = server.address().port;
+  console.log(
+    "Listening at http://localhost:" +
+      port +
+      " exporting the directory " +
+      __dirname
+  );
 });
