0bac6b32cdacdf14b97d2f6b1c56863af5eec1f1
Aravinda Reddy <aravind4532@gmail.com>
2024-03-26 23:31:05 -0400

    Logout added

--- photoShare.jsx
+++ photoShare.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { HashRouter, Route, Switch, Redirect } from 'react-router-dom';
-import { Grid, Typography, Paper } from '@mui/material';
-import './styles/main.css';
-
-import TopBar from './components/topBar/TopBar';
-import UserDetail from './components/userDetail/userDetail';
-import UserList from './components/userList/userList';
-import UserPhotos from './components/userPhotos/userPhotos';
-import LoginRegister from './components/loginRegister/loginRegister';
+import React from "react";
+import ReactDOM from "react-dom";
+import { HashRouter, Route, Switch, Redirect } from "react-router-dom";
+import { Grid, Typography, Paper } from "@mui/material";
+import "./styles/main.css";
+import axios from "axios";
+import TopBar from "./components/topBar/TopBar";
+import UserDetail from "./components/userDetail/userDetail";
+import UserList from "./components/userList/userList";
+import UserPhotos from "./components/userPhotos/userPhotos";
+import LoginRegister from "./components/loginRegister/loginRegister";
 
 class PhotoShare extends React.Component {
   constructor(props) {
@@ -21,18 +21,30 @@ class PhotoShare extends React.Component {
     this.changeLabelOnTopBar = this.changeLabelOnTopBar.bind(this);
     this.changeCurrentLoggedInUser = this.changeCurrentLoggedInUser.bind(this);
     this.toggleLogin = this.toggleLogin.bind(this);
+    this.handleLogout = this.handleLogout.bind(this);
   }
 
   changeLabelOnTopBar(label) {
     this.setState({ labelOnTopBar: label });
   }
-  changeCurrentLoggedInUser(username){
-    this.setState({currentLoggedInUser:username});
+  changeCurrentLoggedInUser(username) {
+    this.setState({ currentLoggedInUser: username });
   }
 
   toggleLogin(isLoggedIn) {
     this.setState({ isLoggedIn });
   }
+  handleLogout = () => {
+    axios
+      .post("/admin/logout")
+      .then(() => {
+        console.log("Logout successful");
+        this.setState({ isLoggedIn: false, currentLoggedInUser: undefined });
+      })
+      .catch((error) => {
+        console.error("Logout error:", error);
+      });
+  };
 
   render() {
     return (
@@ -40,9 +52,13 @@ class PhotoShare extends React.Component {
         <div>
           <Grid container spacing={8}>
             <Grid item xs={12}>
-              <TopBar currentpageLabelOnTopBar={this.state.labelOnTopBar} currentLoggedInUser={this.state.currentLoggedInUser}/>
+              <TopBar
+                currentpageLabelOnTopBar={this.state.labelOnTopBar}
+                currentLoggedInUser={this.state.currentLoggedInUser}
+                handleLogout={this.handleLogout}
+              />
             </Grid>
-            <div className="main-topbar-buffer"/>
+            <div className="main-topbar-buffer" />
             {this.state.isLoggedIn && (
               <Grid item sm={3}>
                 <Paper className="main-grid-item">
@@ -54,25 +70,57 @@ class PhotoShare extends React.Component {
               <Paper className="main-grid-item">
                 <Switch>
                   <Route exact path="/">
-                    {this.state.isLoggedIn ? 
-                      <Typography variant="body1">Welcome to your photosharing app!</Typography>
-                      : <Redirect to="/admin/login" />}
+                    {this.state.isLoggedIn ? (
+                      <Typography variant="body1">
+                        Welcome to your photosharing app!
+                      </Typography>
+                    ) : (
+                      <Redirect to="/admin/login" />
+                    )}
                   </Route>
-                  <Route path="/users/:userId" render={props => (
-                    this.state.isLoggedIn ? 
-                      <UserDetail {...props} labelOnTopBar={this.changeLabelOnTopBar} />
-                      : <Redirect to="/admin/login" />
-                  )} />
-                  <Route path="/photos/:userId" render={props => (
-                    this.state.isLoggedIn ? 
-                      <UserPhotos {...props} labelOnTopBar={this.changeLabelOnTopBar} />
-                      : <Redirect to="/admin/login" />
-                  )} />
-                  <Route path="/admin/login" render={props => (
-                    this.state.isLoggedIn ? 
-                      <Redirect to="/" />
-                      : <LoginRegister {...props} toggleLogin={this.toggleLogin} changeCurrentLoggedInUser={this.changeCurrentLoggedInUser} />
-                  )} />
+                  <Route
+                    path="/users/:userId"
+                    render={(props) =>
+                      this.state.isLoggedIn ? (
+                        <UserDetail
+                          {...props}
+                          labelOnTopBar={this.changeLabelOnTopBar}
+                        />
+                      ) : (
+                        <Redirect to="/admin/login" />
+                      )
+                    }
+                  />
+                  <Route
+                    path="/photos/:userId"
+                    render={(props) =>
+                      this.state.isLoggedIn ? (
+                        <UserPhotos
+                          {...props}
+                          labelOnTopBar={this.changeLabelOnTopBar}
+                        />
+                      ) : (
+                        <Redirect to="/admin/login" />
+                      )
+                    }
+                  />
+                  <Route
+                    path="/admin/login"
+                    render={(props) =>
+                      this.state.isLoggedIn ? (
+                        <Redirect to="/" />
+                      ) : (
+                        <LoginRegister
+                          {...props}
+                          toggleLogin={this.toggleLogin}
+                          changeCurrentLoggedInUser={
+                            this.changeCurrentLoggedInUser
+                          }
+                          handleLogout={this.handleLogout}
+                        />
+                      )
+                    }
+                  />
                 </Switch>
               </Paper>
             </Grid>
@@ -83,7 +131,4 @@ class PhotoShare extends React.Component {
   }
 }
 
-ReactDOM.render(
-  <PhotoShare />,
-  document.getElementById('photoshareapp')
-);
+ReactDOM.render(<PhotoShare />, document.getElementById("photoshareapp"));
